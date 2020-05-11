25e8142a26451ecd50e0ebd350b1764e4537f05e
Luka Devic <43738975+LukaDevic@users.noreply.github.com>
2020-05-11 08:41:17 +0200

    Add Product Context and Migration for creation od Products Db

---
+++ MVCCoreApp/MVCCoreApp.Data/Migrations/20200510220824_AddPRoductsDb.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVCCoreApp.Data.Migrations
+{
+    public partial class AddPRoductsDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(maxLength: 20, nullable: false),
+                    Description = table.Column<string>(maxLength: 50, nullable: false),
+                    Category = table.Column<string>(maxLength: 50, nullable: false),
+                    Manufacturer = table.Column<string>(maxLength: 50, nullable: false),
+                    Supplier = table.Column<string>(maxLength: 50, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ MVCCoreApp/MVCCoreApp.Data/ProductsContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCCoreApp.Data.Models;
+
+namespace MVCCoreApp.Data
+{
+    public class ProductsContext : DbContext
+    {
+        public ProductsContext(DbContextOptions<ProductsContext> options)
+        : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
